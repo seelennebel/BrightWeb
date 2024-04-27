e44e0887f93c7e7f96c8291c81fc0b7ec2e9f6ac
seelennebel <mr.andrey.amitan@gmail.com>
2024-04-28 00:29:21 +0200

    changes

--- front_end/src/components/CartProduct.vue
+++ front_end/src/components/CartProduct.vue
@@ -1,69 +1,76 @@
 <template>
-    <div id = "container" v-for = "product in fin.value" :key = product._id>
-			<div id = "image">
-				<img v-if = "product.file != undefined" :src = "'http://localhost:8000/api/image/' + product.file">
-				<div v-else></div>
+    <div id = "container" class = "flex-row" v-for = "product in props.products" :key = product[0]._id>
+		<div id = "image">
+			<img :src = "'http://localhost:8000/api/image/' + product[0].file">
+		</div>
+		<div id = right-container>
+			<div id = "name" class = "flex-column">
+				<h1 class = "desc"> {{ product[0].name }} </h1>
+				<h2 class = "desc">BRAND: {{ product[0].brand }} </h2>
 			</div>
-			<div id = lower-container class = "flex-row">
-				<div id = "name" class = "flex-column">
-					<p class = "desc">BRAND: {{ product.brand }} </p>
-					<p class = "desc"> {{ product.name }} </p>
-				</div>
-				<div id = "price" class = "flex-column">
-					<p>PRICE</p>
-					<p> {{ product.price }} EUR</p>
-				</div>
+			<div id = "price">
+				<h2>PRICE:</h2>
+				<h2> {{ product[0].price }} EUR</h2>
 			</div>
+			<label id = "quant-label" for = "quantity"> QUANTITY </label>
+			<input type = "number" name = "quantity" id = "quant">
+		</div>
     </div>
 </template>
 
 <script setup>
-import { useStore } from "vuex";
-import { onBeforeMount, defineProps, ref , onMounted} from "vue";
 
-const store = useStore();
+import { defineProps } from 'vue';
 
-const str = [];
-
-for(let i = 0; i < store.state.cart.length; ++i) {
-	str.push(store.state.cart[i]);
-}
-
-const create = async () => {
-	let displayed_products = [];
-	const prods = await Promise.all(
-        str.map(async (productID) => {
-            const res = await fetch(`http://localhost:8000/api/product/${productID}`);
-            const data = await res.json();
-            return data;
-        })
-    );
-	for(let i = 0; i < prods.length; ++i) {
-		displayed_products.push(prods[i][0]);
-	}
-
-	return displayed_products;
-};
-
-const final = await create();
-
-const final_list = ref(final);
-
-let list = [];
-
-for(let product of final_list.value) {
-	let { brand, description, file, name, price, __v, _id } = product; 
-	list.push( { brand, description, file, name, price, __v, _id } )
-}
-
-const fin = ref(list);
-
-console.log(fin.value);
-
-const anal = [
-	{
-		name: "anal"
-	}
-]
+const props = defineProps({
+	products: Array
+})
 
 </script>
+
+<style scoped>
+
+#quant-label
+{
+	margin-right: 0.5vw;
+}
+
+#quant
+{
+	font-size: 1.5vw;
+	width: 3vw;
+	height: 1.5vw;
+	padding: 0;
+}
+
+h2
+{
+	font-size: 1.5vw;
+}
+
+h1
+{
+	font-size: 2vw;
+}
+
+img
+{
+	border: 1px solid black;
+	width: 20vw;
+	height: 20vw;
+}
+
+#right-container
+{
+	margin-left: 2vw;
+}
+
+#container
+{
+	margin-left: 5vw;
+	margin-bottom: 5vw;
+	width: 55vw;
+	height: fit-content;
+}
+
+</style>
--- front_end/src/views/CartView.vue
+++ front_end/src/views/CartView.vue
@@ -1,9 +1,64 @@
 <template>
-    <CartProduct />
+    <main>
+        <div id = "bar">
+            <NavBar />
+        </div>
+        <div id = "global-container" class = "flex-row">
+            <div id = "left">
+                <CartProduct :products = "prods"/>
+            </div>
+            <div id = "right">
+
+            </div>
+        </div>
+    </main>
+    
 </template>
 
 <script setup>
+import NavBar from "@/components/NavBar.vue";
+import CartProduct from "@/components/CartProduct.vue";
 
-import CartProduct from "../components/CartProduct.vue";
+import { useStore } from "vuex";
+import { ref } from "vue";
+
+const store = useStore();
+
+const prods = ref([]);
+
+const fetch_products = async () => {
+	try {
+		let responses = await Promise.all(store.state.cart.map(async productID => {
+			const response = await fetch(`http://localhost:8000/api/product/${productID}`);
+			return response.json()
+		}))
+		return responses;
+	}
+	catch(error) {
+		console.log(error);
+	}
+}
+
+fetch_products().then(res => prods.value = res);
 
 </script>
+
+<style scoped>
+
+#left
+{
+    width: 60vw;
+}
+
+#right
+{
+    width: 40vw; 
+    border-right: 1px solid black;
+}
+
+#bar
+{
+    margin-bottom: 5vw;
+}
+
+</style>
